aca250ff5d3bd4c60eec9e78cfca4e753d8e3a9b
pu1s <pu1s@outlook.com>
2016-02-19 11:03:47 +0300

    + get

--- 500px.API/500px.API/src/Request.cs
+++ 500px.API/500px.API/src/Request.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using OAuth;
@@ -10,6 +13,54 @@ namespace _500px.API
 {
     public class Request
     {
+        public async Task<T> Get<T>(string signegQwery) where T : Response, new()
+        {
+            T resultValue;
+            var result = new Response();
+            //
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(signegQwery);
+                if(response.IsSuccessStatusCode)
+                {
+                    result.StatusCode = response.StatusCode;
+                    result.Content  = await response.Content.ReadAsStringAsync();
+                    result.IsSuccess = response.IsSuccessStatusCode;
+                    result.Error = string.Empty;
+                }
+                result.Content = string.Empty;
+                result.StatusCode = response.StatusCode;
+                result.Error = "Bad request";
+#if DEBUG
+                Debug.WriteLine(result.Error);
+#endif
+            }
+            resultValue = (T)result;
+            return resultValue;
+        }
 
+        public async Task<Response> Get(string signedQweryString)
+        {
+            var result = new Response();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(signedQweryString);
+                if (response.IsSuccessStatusCode)
+                {
+                    result.StatusCode = response.StatusCode;
+                    result.Content = await response.Content.ReadAsStringAsync();
+                    result.IsSuccess = response.IsSuccessStatusCode;
+                    result.Error = string.Empty;
+                }
+                result.Content = string.Empty;
+                result.StatusCode = response.StatusCode;
+                result.Error = "Bad request";
+#if DEBUG
+                Debug.WriteLine(result.Error);
+#endif
+            }
+            return result;
+        }
+        
     }
 }
--- 500px.API/500px.API/src/Response.cs
+++ 500px.API/500px.API/src/Response.cs
@@ -5,7 +5,7 @@ namespace _500px.API
 {
     public class Response
     {
-        public Content Content { get; set; }
+        public string Content { get; set; }
         public string Error { get; set; }
         public HttpStatusCode StatusCode { get; set; }
         public bool IsSuccess { get; set; }
